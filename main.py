e5ca7b224424ea3b05cc6bea4b95de0323ba6f2a
Ozzyz <aasmundbrekke@gmail.com>
2018-09-04 16:03:20 +0200

    Add working toy cnn architecture

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 
 from model import basic_cnn
 from keras.datasets import mnist
-
+from keras import utils
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+num_classes = 10
+img_rows, img_cols = (28, 28)
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# convert class vectors to binary class matrices
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
 
 
 model = basic_cnn(input_dim=(28, 28, 1))
 
-model.fit(x_train, y_train)
-
-a = model.evaluate(x_test, y_test)
-print(a)
+model.fit(x_train, y_train,
+          validation_data=(x_test, y_test))
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import keras
-from keras.layers import Input, Conv2D, Dense
+from keras.layers import Input, Conv2D, Dense, MaxPooling2D, Flatten
 from keras.models import Model
 
 
@@ -7,15 +7,18 @@ def basic_cnn(input_dim=(512, 512, 3)):
     inputs = Input(shape=input_dim)
 
     conv1 = Conv2D(filters=64, kernel_size=3)(inputs)
-    conv2 = Conv2D(filters=32, kernel_size=3)(conv1)
-
-    fully_connected = Dense(256, activation='relu')(conv2)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(filters=32, kernel_size=3)(pool1)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    flat = Flatten()(pool2)
+    fully_connected = Dense(256, activation='relu')(flat)
     predictions = Dense(10, activation='softmax')(fully_connected)
 
     model = Model(inputs=inputs, outputs=predictions)
     model.compile(optimizer='rmsprop',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
+    print(model.summary())
     return model
 
 
